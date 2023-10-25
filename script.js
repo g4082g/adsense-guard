26d33b1ecab60b786370c87ec77cb295973b355f
guvno <111243244+guvno@users.noreply.github.com>
2023-10-25 20:51:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function BlacklistAccess() {
 
 function addClickCount() {
   let clickCount = parseInt(localStorage.getItem('ProtectionClickCount') || '0') + 1;
+  console.log("Current click count:", clickCount);
   clickCount > 3 && (BlacklistAccess(), (clickCount = 0));
   localStorage.setItem('ProtectionClickCount', clickCount.toString());
 }
@@ -43,11 +44,11 @@ function checkBlacklist() {
 }
 
 function showProtectionLog() {
-  console.log('애드센스 가드 무효 트래픽 감시 중\\n무효 광고 클릭을 차단합니다.');
+  console.log('애드센스 무효 트래픽 감시 중\\n무효 광고 클릭을 차단합니다.');
 }
 
 function showAlert() {
-  alert('애드센스 무효 클릭 공격이 감지되었습니다.\\n IP를 수집합니다.');
+  alert('애드센스 무효 클릭 공격이 감지되었습니다.\\n IP는 사이트 소유자에게 기록됩니다.\\n 무효 광고 클릭 반복 시 법적 책임을 질 수 있습니다.');
   window.location.replace('https://www.tistory.com/');
 }
 
